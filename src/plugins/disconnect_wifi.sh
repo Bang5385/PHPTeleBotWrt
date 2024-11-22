69d491cd3bbe748396cad781add38e54231f7c7a
Bang5385 <93089764+Bang5385@users.noreply.github.com>
2024-11-22 10:25:41 +0700

    Update disconnect_wifi.sh

--- src/plugins/disconnect_wifi.sh
+++ src/plugins/disconnect_wifi.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # Nhận địa chỉ MAC từ tham số đầu vào
 MAC=$1
 
 # Kiểm tra nếu MAC không trống
 if [ -z "$MAC" ]; then
-    echo -e "Không có địa chỉ MAC"
+    echo "Không có địa chỉ MAC"
     exit 1
 fi
 
@@ -13,4 +13,4 @@ fi
 iw dev $INTERFACE1 station del $MAC
 iw dev $INTERFACE2 station del $MAC
 
-echo -e "Đã ngắt kết nối thiết bị với MAC $MAC khỏi Wi-Fi"
+echo "Đã ngắt kết nối thiết bị với MAC $MAC khỏi Wi-Fi"
