149219f3a7fe6f75e4ff5605464e85b83f5da404
Bang5385 <93089764+Bang5385@users.noreply.github.com>
2024-11-22 12:53:49 +0700

    Update disconnect_wifi.sh

--- src/plugins/disconnect_wifi.sh
+++ src/plugins/disconnect_wifi.sh
@@ -9,20 +9,33 @@ fi
 MAC=$1
 INTERFACE1="phy0-ap0"
 INTERFACE2="phy1-ap0"
-# Kiểm tra nếu interface có hỗ trợ lệnh iw dev
-if ! iw dev $INTERFACE station dump > /dev/null 2>&1; then
-  echo "Không tìm thấy giao diện Wi-Fi $INTERFACE."
+
+# Kiểm tra nếu interface1 có hỗ trợ lệnh iw dev
+if ! iw dev $INTERFACE1 station dump > /dev/null 2>&1; then
+  echo "Không tìm thấy giao diện Wi-Fi $INTERFACE1."
   exit 1
 fi
 
-# Kiểm tra xem địa chỉ MAC có đang kết nối không
-CONNECTED_MAC=$(iw dev $INTERFACE station dump | grep "Station" | awk '{print $2}' | grep -i "$MAC")
+# Kiểm tra nếu interface2 có hỗ trợ lệnh iw dev
+if ! iw dev $INTERFACE2 station dump > /dev/null 2>&1; then
+  echo "Không tìm thấy giao diện Wi-Fi $INTERFACE2."
+  exit 1
+fi
 
-if [ "$CONNECTED_MAC" == "$MAC" ]; then
+# Kiểm tra xem địa chỉ MAC có đang kết nối không ở cả 2 interface
+CONNECTED_MAC1=$(iw dev $INTERFACE1 station dump | grep "Station" | awk '{print $2}' | grep -i "$MAC")
+CONNECTED_MAC2=$(iw dev $INTERFACE2 station dump | grep "Station" | awk '{print $2}' | grep -i "$MAC")
+
+if [ "$CONNECTED_MAC1" == "$MAC" ] || [ "$CONNECTED_MAC2" == "$MAC" ]; then
     # Ngắt kết nối
-    iw dev $INTERFACE1 disconnect
-    iw dev $INTERFACE2 disconnect
-    echo "Đã ngắt kết nối thiết bị với MAC $MAC khỏi Wi-Fi."
+    if [ "$CONNECTED_MAC1" == "$MAC" ]; then
+        iw dev $INTERFACE1 disconnect
+        echo "Đã ngắt kết nối thiết bị với MAC $MAC khỏi giao diện $INTERFACE1."
+    fi
+    if [ "$CONNECTED_MAC2" == "$MAC" ]; then
+        iw dev $INTERFACE2 disconnect
+        echo "Đã ngắt kết nối thiết bị với MAC $MAC khỏi giao diện $INTERFACE2."
+    fi
 else
     echo "Không tìm thấy thiết bị với MAC $MAC đang kết nối."
     exit 1
